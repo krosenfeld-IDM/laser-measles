06802afb99841b9e0c407d5ff29dc74b7c054111
Katherine <krosenf@gmail.com>
2025-06-06 18:53:53 +0000

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,58 +1,137 @@
+# -*- coding: utf-8 -*-
+#
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+import sciris as sc
+
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'laser-measles'
+copyright = f'2024 - {sc.now().year}, Bill & Melinda Gates Foundation. All rights reserved.'
+
+# The short X.Y version
+version = "0.6.0"
+
+# The full version, including alpha/beta/rc tags
+release = version
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.coverage",
-    "sphinx.ext.doctest",
-    "sphinx.ext.extlinks",
-    "sphinx.ext.ifconfig",
+    "sphinx.ext.autodoc",  # Core Sphinx library for auto html doc generation from docstrings
+    "sphinx.ext.autosummary",  # Create neat summary tables for modules/classes/methods etc -- causes warnings with Napoleon however
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",  # Add a link to the Python source code for classes, functions etc.
     "sphinx.ext.napoleon",
-    "sphinx.ext.todo",
-    "sphinx.ext.viewcode",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.doctest",  # Enable doctest builder
+    "sphinx_autodoc_typehints",  # Automatically document param types (less noise in class signature)
+    "sphinx_design", # Add e.g. grid layout
+    'sphinx_search.extension', # search across multiple docsets in domain
+    "nbsphinx",
 ]
-source_suffix = {".rst": "restructuredtext"}
-master_doc = "index"
-project = "laser_measles"
-year = "2024"
-author = "Institute for Disease Modeling"
-copyright = f"{year}, Bill & Melinda Gates Foundation"
-version = release = "0.6.0"
 
-pygments_style = "trac"
-templates_path = ["."]
-extlinks = {
-    "issue": ("https://github.com/InstituteforDiseaseModeling/laser-measles/issues/%s", "#%s"),
-    "pr": ("https://github.com/InstituteforDiseaseModeling/laser-measles/pull/%s", "PR #%s"),
-}
-
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    # "githuburl": "https://github.com/InstituteforDiseaseModeling/laser-measles/",
-}
-
-html_use_smartypants = True
-html_last_updated_fmt = "%b %d, %Y"
-html_split_index = False
-html_short_title = f"{project}-{version}"
-
-# Napoleon settings (Napolean converts Google-style docstrings to reStructuredText)
+# Use Google docstrings
 napoleon_google_docstring = True
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = True  # from Cookiecutter template, False is the default
-napoleon_use_param = False  # from Cookiecutter template, True is the default
-napoleon_use_rtype = False  # from Cookiecutter template, True is the default
-napoleon_preprocess_types = False
-napoleon_type_aliases = None
-napoleon_attr_annotations = True
 
-mathjax3_config = {"TeX": {"Macros": {"small": ["{\\scriptstyle #1}", 1]}}}
+# Configure autosummary
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+autosummary_ignore_module_all = False # Respect __all__
+autodoc_member_order = 'bysource' # Keep original ordering
+add_module_names = False  # NB, does not work
+autodoc_inherit_docstrings = False # Stops sublcasses from including docs from parent classes
 
-# Prevent the following warning:
-# sphinx/builders/linkcheck.py:86: RemovedInSphinx80Warning: The default value for 'linkcheck_report_timeouts_as_broken' will change to False in Sphinx 8, meaning that request timeouts will be reported with a new 'timeout' status, instead of as 'broken'. This is intended to provide more detail as to the failure mode. See https://github.com/sphinx-doc/sphinx/issues/11868 for details.
-#   warnings.warn(deprecation_msg, RemovedInSphinx80Warning, stacklevel=1)
-linkcheck_report_timeouts_as_broken = False
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# Syntax highlighting style
+pygments_style = "sphinx"
+modindex_common_prefix = ["laser_measles."]
+
+# List of patterns, relative to source directory, to exclude
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+
+# Suppress certain warnings
+suppress_warnings = ['autosectionlabel.*']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "collapse_navigation": False,
+    "navigation_depth": 3,
+    "show_prev_next": True,
+    "icon_links": [
+        {"name": "Web", "url": "https://starsim.org", "icon": "fas fa-home"},
+        {
+            "name": "GitHub",
+            "url": "https://github.com/InstituteforDiseaseModeling/laser-measles",
+            "icon": "fab fa-github-square",
+        },
+    ],
+    "navbar_end": ["theme-switcher", "navbar-icon-links"],
+    "secondary_sidebar_items": ["navbar-side"],
+    "header_links_before_dropdown": 5,
+    "footer_start": ["copyright", "footer_start"],
+    "footer_end": ["theme-version", "footer_end"],
+}
+html_sidebars = {
+    "**": ["sidebar-nav-bs", "page-toc"],
+}
+html_logo = "images/idm-logo-transparent.png"
+html_favicon = "images/favicon.ico"
+html_static_path = ['_static']
+html_baseurl = "https://docs.idmod.org/"
+html_context = {
+    'rtd_url': 'https://docs.idmod.org/',
+    "versions_dropdown": {
+        "latest": "devel (latest)",
+        "stable": "current (stable)",
+    },
+    "default_mode": "light",
+}
+# Add any extra paths that contain custom files
+if not on_rtd:
+    html_extra_path = ['robots.txt']
+
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_last_updated_fmt = '%Y-%b-%d'
+html_show_sourcelink = True
+html_show_sphinx = False
+html_copy_source = False
+htmlhelp_basename = 'laser-measles'
+
+# Add customizations
+def setup(app):
+    app.add_css_file("theme_overrides.css")
+
+
+# Modify this to not rerun the Jupyter notebook cells -- usually set by build_docs
+nbsphinx_execute = 'always'
+nbsphinx_timeout = 300
+
+# OpenSearch options
+html_use_opensearch = 'docs.idmod.org/projects/laser-measles/en/latest'
+
+# -- RTD Sphinx search for searching across the entire domain, default child -------------
+if os.environ.get('READTHEDOCS') == 'True':
+
+    search_project_parent = "institute-for-disease-modeling-idm"
+    search_project = os.environ["READTHEDOCS_PROJECT"]
+    search_version = os.environ["READTHEDOCS_VERSION"]
+
+    rtd_sphinx_search_default_filter = f"subprojects:{search_project}/{search_version}"
+
+    rtd_sphinx_search_filters = {
+        "Search this project": f"project:{search_project}/{search_version}",
+        "Search all IDM docs": f"subprojects:{search_project_parent}/{search_version}",
+    }

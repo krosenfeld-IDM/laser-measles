fd5f6ca6dae0ca38e5c29c4dba2209f4ae8d24aa
Katherine <krosenf@gmail.com>
2025-06-12 22:36:13 +0000

    Add ImportationPressureProcess component and update __all__ exports; modify importation pressure calculation logic and parameters for improved functionality.

--- src/laser_measles/biweekly/components/__init__.py
+++ src/laser_measles/biweekly/components/__init__.py
@@ -1,7 +1,8 @@
 from .infection import InfectionProcess
 from .vital_dynamics import VitalDynamicsProcess
+from .importation_pressure_process import ImportationPressureProcess
 
-__all__ = ["InfectionProcess", "VitalDynamicsProcess"]
+__all__ = ["InfectionProcess", "VitalDynamicsProcess", "ImportationPressureProcess"]
 
 from .state_tracker import StateTracker
 from .fadeout_tracker import FadeOutTracker
--- src/laser_measles/biweekly/components/importation_pressure_process.py
+++ src/laser_measles/biweekly/components/importation_pressure_process.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-
 from laser_measles.biweekly.base import BaseComponent
+
+def cast_type(a, dtype):
+    return a.astype(dtype) if a.dtype != dtype else a
 
 
 class ImportationPressureProcess(BaseComponent):
@@ -11,19 +13,25 @@ class ImportationPressureProcess(BaseComponent):
 
     def __init__(self, model, verbose: bool = False) -> None:
         super().__init__(model, verbose)
-        self.crude_importation_rate = 0.001 # N per year per 1k
+        self.crude_importation_rate = 1 # N per year per 1k
+        self.importation_start = 0
+        self.importation_end = 4 # bi weeks
 
     def __call__(self, model, tick: int) -> None:
+        if tick < self.importation_start or tick > self.importation_end:
+            return
+
         # state counts
         states = model.nodes.states
 
-        # model parameters
-        params = model.params
+        # population
+        population = states.sum(axis=0)
 
         # importation pressure
-        importation_pressure = states * (self.params.crude_importation_rate / 26.0 / 1000.0)
+        importation_pressure = population * (self.crude_importation_rate / 26.0 / 1000.0)
 
         # ensure importation pressure is not greater than the susceptible population
+        importation_pressure = cast_type(importation_pressure, states.dtype)
         np.minimum(importation_pressure, states[0], out=importation_pressure)
 
         # update states
